48cc4d4ba2f1df7b1e49e0e06c12e0f981a799bd
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-06-06 13:07:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,20 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://todo-fe-mu.vercel.app', 'https://todo-fe-mu.vercel.app/register');
+  next();
+});
 app.use(cors({
   origin: 'https://todo-fe-mu.vercel.app/',
   credentials: true
 }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://todo-fe-mu.vercel.app', 'https://todo-fe-mu.vercel.app/register');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  next();
-});
 
 connectDB();
 
