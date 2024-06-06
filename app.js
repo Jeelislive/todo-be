3a6c00de5ae68d4c9ec63232f91d84d84f7878a0
JEEL RUPARELIYA <jeelrupareliya255@gmail.com>
2024-06-06 11:41:33 +0530

    .

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://todo-fe-mu.vercel.app/register',
+  origin: 'https://todo-fe-mu.vercel.app',
   credentials: true 
 }));
 
