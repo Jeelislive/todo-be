4980b670fa428831dcf92186cc1d15327a110af0
JEEL RUPARELIYA <jeelrupareliya255@gmail.com>
2024-06-06 11:54:29 +0530

    cors headers added

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use(cors({
 
 connectDB();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://todo-fe-mu.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>App is working</h1>");
 });
